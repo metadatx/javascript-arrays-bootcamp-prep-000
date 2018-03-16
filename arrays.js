0e9029cb2ac52977a4b5c62fc1e94065d1defd2c
FIRAT AKSU <aksu_fr@yahoo.es>
2018-03-16 17:56:49 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,34 @@
+var chocolateBars = ["snickers","hundred grand", "kitkat", "skittles"]
+
+function addElementToBeginningOfArray(arr, elm){
+  var arr2 = [elm, ...arr]
+  return arr2
+}
+
+function destructivelyAddElementToBeginningOfArray(arr, elm){
+  arr.push(elm)
+}
+
+function accessElementInArray(arr, index){
+  return arr[index]
+}
+
+function destructivelyRemoveElementFromBeginningOfArray(arr){
+  arr.shift()
+  return arr
+}
+
+function removeElementFromBeginningOfArray(arr){
+  return arr.slice(1)
+}
+
+function destructivelyRemoveElementFromEndOfArray(arr){
+  arr.pop()
+  return arr
+}
+
+function removeElementFromEndOfArray(arr){
+  return arr.slice(0, arr.length - 1)
+}
+
+
